0dd6d037859100100dc7bae23768de762ab63471
Flora Soler <florasoler@floras-mbp.home>
2024-11-01 10:48:27 +0100

    Création API RESTful complétée

--- backend/app.js
+++ backend/app.js
@@ -2,18 +2,15 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const Book = require('./models/book');
 
-const app = express();
-
-mongoose.connect('mongodb+srv://floralusoler:<db_password>@clusteroc.o6o5d.mongodb.net/?retryWrites=true&w=majority&appName=ClusterOC',
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://floralusoler:RjVIcpqHvLqP8tCd@clusteroc.o6o5d.mongodb.net/?retryWrites=true&w=majority&appName=ClusterOC')
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-app.use(express.json()); // Intercepte toutes les requêtes qui ont du contenu JSON
+const app = express();
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -23,26 +20,45 @@ app.use((req, res, next) => {
     next();
   });
 
-  app.post('/api/books', (req, res, next) => {
-    delete req.body._id; // on supprime le faux id envoyé par le front-end
-    const book = new Book({
-      ...req.body
-    });
-    book.save()
-      .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-      .catch(error => res.status(400).json({ error }));
-  });
+app.use(bodyParser.json()); // Rend les données du corps de la requête exploitables
 
-app.get('/api/books',(req, res) => {
-  fs.readFile(path.join(__dirname, '../frontend/public/data/data.json'), 'utf8', (err, data) => {
-      if (err) {
-          console.error('Error reading the data file', err);
-          res.status(500).json({ message: 'Server error' });
-          return;
-      }
-      const books = JSON.parse(data);
-      res.status(200).json(books);
+// Route POST
+app.post('/api/books', (req, res, next) => {
+  delete req.body._id;  // on supprime le faux id envoyé par le front-end
+  const book = new Book({
+    ...req.body         // L'opérateur spread ... est utilisé pour faire une copie de tous les éléments de req.body
   });
+  book.save()           // enregistre new Book dans la base de données
+    .then(() => res.status(201).json({ message: 'Livre enregistré !'})) // on envoie une réponse, sinon la requête expire
+    .catch(error => res.status(400).json({ error }));
+});
+
+// Route GET individuelle
+app.get('/api/books/:id', (req, res, next) => {     
+  Book.findOne({ _id: req.params.id })                  // paramètre de route dynamique
+    .then(book => res.status(200).json(book))
+    .catch(error => res.status(404).json({ error }));
+});
+
+// Route PUT 
+app.put('/api/books/:id', (req, res, next) => {
+  Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Livre modifié !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+// Route DELETE
+app.delete('/api/books/:id', (req, res, next) => {
+  Book.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Livre supprimé !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+// Route GET 
+app.get('/api/books',(req, res) => {
+  Book.find()
+    .then(books => res.status(200).json(books))
+    .catch(error => res.status(400).json({ error }));
 });
 
 module.exports = app;
